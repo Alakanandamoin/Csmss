63cd2bf4f70c1141a1da03599549929ce6e7532b
Alakanandamoin <143710447+Alakanandamoin@users.noreply.github.com>
2023-09-02 09:45:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document.getElementById('myBtn').addEventListener('click',getdata);
+fuction getData(){
+    //console.log(''test');
+    //Get API
+    fetch("https://randomuser.me/api/")
+    
+}
